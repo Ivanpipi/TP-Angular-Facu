64d56cf212f216c7c3a35a9cea1b1252026b4905
Ivanpipi <ivandavidpipitone@gmail.com>
2021-09-14 21:52:24 -0300

    tp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { CargaDatosComponent } from './carga-datos/carga-datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    CargaDatosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'login-page', component: LoginPageComponent },
+      { path: 'register-page', component: RegisterPageComponent },
+      { path: 'carga-datos', component: CargaDatosComponent },
+
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/carga-datos/carga-datos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carga-datos',
+  templateUrl: './carga-datos.component.html',
+  styleUrls: ['./carga-datos.component.scss']
+})
+export class CargaDatosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
